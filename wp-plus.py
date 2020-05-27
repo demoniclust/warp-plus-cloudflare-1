536aebe96886397bed398c5d78f25efd0e206bd5
ALI-B <babab7526@gmail.com>
2020-05-28 02:03:52 +0430

    fix connection

--- wp-plus.py
+++ wp-plus.py
@@ -4,7 +4,11 @@ import datetime
 import random
 import string
 import time
-print ('      _______ _      __________________       _______ _______ _______ _______\n'
+import os
+import sys
+os.system("title WARP-PLUS-CLOUDFLARE By ALIILAPRO")
+os.system('cls')
+print('      _______ _      __________________       _______ _______ _______ _______\n'
 '     (  ___  | \     \__   __|__   __( \     (  ___  |  ____ |  ____ |  ___  )\n'
 '     | (   ) | (        ) (     ) (  | (     | (   ) | (    )| (    )| (   ) |\n'
 '     | (___) | |        | |     | |  | |     | (___) | (____)| (____)| |   | |\n'
@@ -12,46 +16,75 @@ print ('      _______ _      __________________       _______ _______ _______ __
 '     | (   ) | |        | |     | |  | |     | (   ) | (     | (\ (  | |   | |\n'
 '     | )   ( | (____/\__) (_____) (__| (____/\ )   ( | )     | ) \ \_| (___) |\n'
 '     |/     \(_______|_______|_______(_______//     \|/      |/   \__(_______)\n')
-print ("-----------------------------------------------------------------------------")
-print ("[?] Q; How to find a WARP+ ID?")
-print ("[-] A; Go this route and copy the WARP+ ID => Setting/Advanced/Diagnostics/ID")
-print ("-----------------------------------------------------------------------------")
-print ("This script is coded by aliilapro [ aliilapro.github.io ]") 
-print ("-----------------------------------------------------------------------------")
-referrer = input("Enter the WARP+ ID: ")
+print ("[+] ABOUT SCRIPT:")
+print ("[-] With this script, you can getting unlimited GB on Warp+.")
+print ("--------")
+print ("[+] THIS SCRIPT CODDED BY ALIILAPRO") 
+print ("[-] SITE: aliilapro.github.io") 
+print ("[-] TELEGRAM: aliilapro")
+print ("--------")
+referrer = input("[#] Enter the WARP+ ID:")
 def genString(stringLength):
-    letters = string.ascii_letters + string.digits
-    return ''.join(random.choice(letters) for i in range(stringLength))
+	try:
+		letters = string.ascii_letters + string.digits
+		return ''.join(random.choice(letters) for i in range(stringLength))
+	except Exception as error:
+		print(error)		    
 def digitString(stringLength):
-    digit = string.digits
-    return ''.join((random.choice(digit) for i in range(stringLength)))    
-url = f'http://api.cloudflareclient.com/v0a{digitString(3)}/reg'
+	try:
+		digit = string.digits
+		return ''.join((random.choice(digit) for i in range(stringLength)))    
+	except Exception as error:
+		print(error)    
+url = f'https://api.cloudflareclient.com/v0a{digitString(3)}/reg'
 def run():
-    install_id = genString(11)
-    body = {"key": "{}=".format(genString(42)),
-            "install_id": install_id,
-            "fcm_token": "{}:APA91b{}".format(install_id, genString(134)),
-            "referrer": referrer,
-            "warp_enabled": False,
-            "tos": datetime.datetime.now().isoformat()[:-3] + "+07:00",
-            "type": "Android",
-            "locale": "zh-CN"}
-    bodyString = json.dumps(body)
-    headers = {'Content-Type': 'application/json; charset=UTF-8',
-               'Host': 'api.cloudflareclient.com',
-               'Connection': 'Keep-Alive',
-               'Accept-Encoding': 'gzip',
-               'User-Agent': 'okhttp/3.12.1'
-               }
-    r = requests.post(url, data=bodyString, headers=headers)
-    return r
-c = 1
-while True:
-    result = run()
-    if result.status_code == 200:
-        print(f"\n{c} GB has been successfully added to your account.")
-        c = c + 1
-        time.sleep(20)
-    else:
-        print("Error when connecting to server.")
+	try:
+		install_id = genString(11)
+		body = {"key": "{}=".format(genString(42)),
+				"install_id": install_id,
+				"fcm_token": "{}:APA91b{}".format(install_id, genString(134)),
+				"referrer": referrer,
+				"warp_enabled": False,
+				"tos": datetime.datetime.now().isoformat()[:-3] + "+07:00",
+				"type": "Android",
+				"locale": "zh-CN"}
+		bodyString = json.dumps(body)
+		headers = {'Content-Type': 'application/json; charset=UTF-8',
+					'Host': 'api.cloudflareclient.com',
+					'Connection': 'Keep-Alive',
+					'Accept-Encoding': 'gzip',
+					'User-Agent': 'okhttp/3.12.1'
+					}
+		r = requests.post(url, data=bodyString, headers=headers)
+		return r
+	except Exception as error:
+		print(error)	
 
+g = 0
+b = 0
+while True:
+	result = run()
+	if result.status_code == 200:
+		g = g + 1
+		os.system('cls')
+		print("")
+		print("                  WARP-PLUS-CLOUDFLARE (script)" + " By ALIILAPRO")
+		print("")
+		animation = ["[■□□□□□□□□□] 10%","[■■□□□□□□□□] 20%", "[■■■□□□□□□□] 30%", "[■■■■□□□□□□] 40%", "[■■■■■□□□□□] 50%", "[■■■■■■□□□□] 60%", "[■■■■■■■□□□] 70%", "[■■■■■■■■□□] 80%", "[■■■■■■■■■□] 90%", "[■■■■■■■■■■] 100%"] 
+		for i in range(len(animation)):
+			time.sleep(0.5)
+			sys.stdout.write("\r[+] Preparing... " + animation[i % len(animation)])
+			sys.stdout.flush()
+		print(f"\n[-] WORK ON ID: {referrer}")    
+		print(f"[:)] {g} GB has been successfully added to your account.")
+		print(f"[#] Total: {g} Good {b} Bad")
+		print("[*] After 18 seconds, a new request will be sent.")
+		time.sleep(18)
+	else:
+		b = b + 1
+		os.system('cls')
+		print("")
+		print("                  WARP-PLUS-CLOUDFLARE (script)" + " By ALIILAPRO")
+		print("")
+		print("[:(] Error when connecting to server.")
+		print(f"[#] Total: {g} Good {b} Bad")	
